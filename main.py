8530b81d3c0eb2c1a3021f29e0820801db13a610
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2023-03-07 02:15:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
-import discord
 import os
-from discord.ext import commands
+import sys
+import json
+import time
+import requests
+import websocket
 from keep_alive import keep_alive
 
-client=commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+status = "online"
 
-GUILD_ID = YOUR_SERVER_ID_HERE
-CHANNEL_ID = YOUR_CHANNEL_ID_HERE
+GUILD_ID = ADD_YOUR_SERVER_ID_HERE
+CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+SELF_MUTE = True
+SELF_DEAF = False
 
-@client.event
-async def on_ready():
-    os.system('clear')
-    print(f'Logged in as {client.user} ({client.user.id})')
-    vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id = CHANNEL_ID)
-    await vc.guild.change_voice_state(channel=vc, self_mute=False, self_deaf=False)
-    print(f"Successfully joined {vc.name} ({vc.id})")
+usertoken = os.getenv("TOKEN")
+if not usertoken:
+  print("[ERROR] Please add a token inside Secrets.")
+  sys.exit()
+
+headers = {"Authorization": usertoken, "Content-Type": "application/json"}
+
+validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
+if validate.status_code != 200:
+  print("[ERROR] Your token might be invalid. Please check it again.")
+  sys.exit()
+
+userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+
+def joiner(token, status):
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    start = json.loads(ws.recv())
+    heartbeat = start['d']['heartbeat_interval']
+    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
+    ws.send(json.dumps(auth))
+    ws.send(json.dumps(vc))
+    time.sleep(heartbeat / 1000)
+    ws.send(json.dumps({"op": 1,"d": None}))
+
+def run_joiner():
+  os.system("clear")
+  print(f"Logged in as {username}#{discriminator} ({userid}).")
+  while True:
+    joiner(usertoken, status)
+    time.sleep(30)
 
 keep_alive()
-client.run(os.getenv("TOKEN"))
+run_joiner()
