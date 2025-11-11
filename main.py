19943c65b3ef9d1fb506231dc997c0b35707f01f
dtrai1024-commits <dtrai1024@gmail.com>
2025-11-11 16:20:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1331527427779985448
+CHANNEL_ID = 1432035352238887144
 SELF_MUTE = True
 SELF_DEAF = False
 
